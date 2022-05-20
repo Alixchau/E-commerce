28554d4fbfdf9b2c7a4ed7b14756218824454e0d
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-05-19 22:49:47 -0600

    added get user

--- routes/user.js
+++ routes/user.js
@@ -29,5 +29,24 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
