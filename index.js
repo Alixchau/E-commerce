dd781e094eb6893922134d3488089270160874ac
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-05-19 23:43:54 -0600

    add product route crud

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/user");
+const productsRoute = require("./routes/product");
+
 
 dotenv.config();
 //connect to mongodb database
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connections s
 app.use(express.json());//enable app to pass json
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/products", productsRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
