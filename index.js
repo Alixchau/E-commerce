22d1cbd411f4e9629e485ef318f9f6abd792cdf3
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-05-20 18:51:31 -0600

    added stripe route for payment

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const usersRoute = require("./routes/user");
 const productsRoute = require("./routes/product");
 const cartsRoute = require("./routes/cart");
 const ordersRoute = require("./routes/order");
-
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 //connect to mongodb database
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connections successfull")
 ).catch((e) => console.log(e));
 
+app.use(cors());
 app.use(express.json());//enable app to pass json
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/carts", cartsRoute);
 app.use("/api/orders", ordersRoute);
+app.use("/api/checkout", stripeRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ routes/stripe.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const stripe = require("stripe")("sk_test_51L1ck6D2bTqVrtoS44GV0i0eT1f9IoIuAAc43TXz2SOUgYDFZQ5ivrcLqX2VewOQrQMifYsaxug3wtOxjCiqxZAo00o59l5Ayu");
+
+router.post("/payment", (req,res) =>{
+  //create a charge
+  stripe.charges.create({
+    source:req.body.tokenId,
+    amount:req.body.amount,
+    currency:"usd",
+  },(stripeErr, stripeRes)=>{
+    if(stripeErr){
+      res.status(500).json(stripeErr);
+    }else{
+      res.status(200).json(stripeRes);
+    }
+  });
+});
+module.exports = router;
