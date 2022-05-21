4476047623864c6ac89a64a7a3e02737e1dc81f0
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-05-20 18:48:03 -0600

    added Pay page and connect to create post method to backend stripe route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Cart from './pages/Cart'
+import Cart from './pages/Cart';
+import Pay from "./pages/Pay";
+import PaymentSuccess from "./pages/PaymentSuccess";
 
 const App = () => {
-  return<Cart />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/success" element={ <PaymentSuccess />} />        
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
